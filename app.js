46803f5caed9ea64bbdeb4b3134897f2ee615771
Mariana Ribeiro Gomes <922004@ptin.corpPT.com>
2022-08-10 10:14:07 -0300

    Adicionando console.log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicação conectada ao banco de dados")
+console.log("O Paulo fez esse commit sem você ver na aula")
+console.log("Enviando para o GitHub")
